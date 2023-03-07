2f9691c3f5a5c1ebd74d4ef61959214b2be4fee0
Dan97O <dan97-oddo@live.it>
2023-03-07 13:56:22 +0100

    add result at html name surname and favorite color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
 /* Chiedi all'utente il suo nome */
+let username = prompt("Il tuo nome?");
+console.log(username);
+
 /* Chiedi il suo cognome */
+let lastName = prompt("Il tuo cognome?");
+console.log(lastName);
+
 /* chiedi il suo colore preferito */
+let favoritColor = prompt("Il tuo colore preferito?");
+console.log(favoritColor);
+
 /* Scrivi sulla pagina il risultato
 usando formato: nomecognomecolorepreferito23
 */
+let nameElement = document.getElementById('result');
+console.log(nameElement);
+nameElement.innerHTML = username + lastName + favoritColor + 23;
