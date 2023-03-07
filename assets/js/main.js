1094a4796a875db74bc7f4c8ef27f51261d7c42f
Dan97O <dan97-oddo@live.it>
2023-03-07 13:36:55 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/* Chiedi all'utente il suo nome */
+/* Chiedi il suo cognome */
+/* chiedi il suo colore preferito */
+/* Scrivi sulla pagina il risultato
+usando formato: nomecognomecolorepreferito23
+*/
